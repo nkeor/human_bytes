63433faad18984166ef6186e2a3793d0ae5fdf98
Namkhai B <namkhai.n3@protonmail.com>
2021-04-20 12:51:52 -0500

    refactor(tests): Add tests for `bibytes`
    I moved the tests to a new file, because they were getting a bit long

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 
 //! # Example
 //!
-//! ```
+//! ```no_run
 //! use human_bytes::human_bytes;
 //!
 //! assert_eq!(human_bytes(563_200_u32), "550 KB".to_string());
@@ -16,6 +16,9 @@
 //! // | because `f64` doesn't implement `std::convert::From<u64>`
 //! ```
 //! For more info, check the [README.md](https://gitlab.com/forkbomb9/human_bytes-rs)
+
+#[cfg(test)]
+mod tests;
 
 #[cfg(not(feature = "bibytes"))]
 // Just be future-proof
@@ -53,38 +56,4 @@ pub fn human_bytes<T: Into<f64>>(size: T) -> String {
     result.push_str(SUFFIX[base.floor() as usize]);
 
     result
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn nothing() {
-        assert_eq!(super::human_bytes(0_u32), "0 B".to_string());
-    }
-
-    #[test]
-    fn bytes() {
-        assert_eq!(super::human_bytes(550_u32), "550 B".to_string());
-    }
-    #[test]
-    fn kilobytes() {
-        assert_eq!(super::human_bytes(563_200_u32), "550 KB".to_string());
-    }
-
-    #[test]
-    fn megabytes() {
-        assert_eq!(super::human_bytes(681_574_400_u32), "650 MB".to_string());
-    }
-
-    #[test]
-    fn gigabytes() {
-        assert_eq!(super::human_bytes(16_428_249_907_u64 as f64), "15.3 GB".to_string());
-    }
-
-    #[test]
-    fn terabytes() {
-        // Hacky, I know, but easier to write ;)
-        let terabyte: u64 = 2_u64.pow(40);
-        assert_eq!(super::human_bytes(((terabyte * 2) + (terabyte / 2)) as f64), "2.5 TB");
-    }
 }
